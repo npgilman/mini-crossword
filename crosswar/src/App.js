48b8289d74bd5c7e51d792aaa5c4b180b8ede112
cjannun <93235878+cjannun@users.noreply.github.com>
2023-04-27 16:06:23 -0400

    Hardcoded opponent status board, documented all my changes

--- crosswar/src/App.js
+++ crosswar/src/App.js
@@ -4,6 +4,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* Opponent status board */}
+        <div id="opponent-board">
+          <p id="opponent-row">
+            ⬛⬜⬜⬜⬜
+          </p>
+          <p id="opponent-row">
+            ⬜⬜⬜⬜⬜
+          </p>
+          <p id="opponent-row">
+            ⬜⬜⬜⬜⬜
+          </p>
+          <p id="opponent-row">
+            ⬜⬜⬜⬜⬛
+          </p>
+          <p id="opponent-row">
+            ⬜⬜⬜⬛⬛
+          </p>
+        </div>
+
+        {/* Spacing between opponent board and puzzle */}
+        <div>
+          <br></br>
+          <br></br>
+        </div>
+        
+        {/* 5x5 grid of square input elements */}
+        {/* Each box can contain a single uppercase letter (see App.css #letter-box) */}
         <div name="row-1">
           <input name="MyInput" id="letter-box" maxlength="1" />
           <input name="MyInput" id="letter-box" maxlength="1" />

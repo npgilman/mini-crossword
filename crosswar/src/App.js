28594e2a36eec5a383cffe9bc31f0152cb016877
cjannun <93235878+cjannun@users.noreply.github.com>
2023-04-27 15:08:19 -0400

    Installed React and made letter boxes

---
+++ crosswar/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div name="row-1">
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+        </div>
+        <div name="row-2">
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+        </div>
+        <div name="row-3">
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+        </div>
+        <div name="row-4">
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+        </div>
+        <div name="row-5">
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+          <input name="MyInput" id="letter-box" maxlength="1" />
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

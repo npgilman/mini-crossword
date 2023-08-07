b9fe2b7908ea86c7f993ab79facf34c3d7ea0d39
dpayne77 <100661623+dpayne77@users.noreply.github.com>
2023-08-06 20:07:08 -0400

    Better Navbar & Responsive Grid sizes

--- crosswar/src/components/CrosswarNav.js
+++ crosswar/src/components/CrosswarNav.js
@@ -4,15 +4,25 @@ export default function CrosswarNav() {
   return (
     <>
         <div id="CrossNav">
-            <ul id="WebsiteTitle">
-                <li>CrossWar</li>
-            </ul>
-            <ul className='clickable'>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Leaderboards</a></li>
-                <li><a href="#">Host Game</a></li>
-                <li><a href="#"> Join Game</a></li>
-            </ul>
+          <table style={{width: "100%", height: "100%"}}>
+            <tr>
+              <td style={{width: "35%"}}>
+                CrossWar
+              </td>
+              <td className='clickable'>
+                <a href="#">Join Game</a>
+              </td>
+              <td className='clickable'>
+                <a href="#">Host Game</a>
+              </td>
+              <td className='clickable'>
+                <a href="#">Leaderboards</a>
+              </td>
+              <td className='clickable'>
+                <a href="#">Profile</a>
+              </td>
+            </tr>
+          </table>
         </div>
     </>
   )

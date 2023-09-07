c7ef3cada09dbc66b3d0547f63e3bdf918f25a87
cjannun <93235878+cjannun@users.noreply.github.com>
2023-09-06 23:12:57 -0400

    Python script to test crossword generation time

---
+++ crosswar/src/Crossword Generator/timeTest.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+
+# Number of times to run test
+num_runs = 20
+
+total_execution_time = 0
+
+for i in range(num_runs):
+  start_time = time.time()
+  
+  output = subprocess.run(["node", "index.js"], stdout=subprocess.DEVNULL)
+  
+  end_time = time.time()
+  
+  # Execution time for current run
+  curr_execution_time = end_time - start_time
+  
+  # Update total execution time
+  total_execution_time += curr_execution_time
+
+# Calculate the average execution time
+average_execution_time = total_execution_time / num_runs
+
+print(f'Average execution time over {num_runs} runs: {average_execution_time:.2f} seconds')
